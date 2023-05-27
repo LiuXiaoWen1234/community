a975208e2883cf568e6bae7bb4e8650045910cce
liuxiaowen <3497175414@qq.com>
2023-05-27 15:10:56 +0800

    2.27显示登录信息

---
+++ src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
@@ -0,0 +1,32 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.LoginTicket;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface LoginTicketMapper {
+    //插入功能
+    @Insert({
+            "insert into login_ticket(user_id, ticket, status, expired) ",
+            "values(#{userId}, #{ticket}, #{status}, #{expired})"
+    })
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insertloginTicket(LoginTicket loginTicket);
+
+
+    //查询功能，依据ticket来查询，把它发给浏览器保存，客户端cookie存储
+    @Select({
+            "select id, user_id, ticket, status, expired ",
+            "from login_ticket where ticket=#{ticket}"
+    })
+    LoginTicket selectByTicket(String ticket);
+
+
+    //修改数据状态
+    @Update({
+            "update login_ticket set status=#{status} where ticket=#{ticket}"
+    })
+    int updateStatus(String ticket, int status);
+
+
+}
